74f80880424e22c99cba35e36253c49e1287164b
Sergio Rubio <sergio@rubio.name>
2009-07-08 16:28:46 +0200

    * misc fixes

--- prowlsms.rb
+++ prowlsms.rb
@@ -1,18 +1,30 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/captcha'
 require 'prowl'
 require 'yaml'
 require 'haml'
 
-config_file = File.join(File.dirname(__FILE__), 'config')
+config_file = File.join(File.dirname(__FILE__), 'config/settings')
 config = YAML.load_file(config_file)
 
+module ProwlSMS
+  def self.config
+    return @@config if defined? @@config 
+    config_file = File.join(File.dirname(__FILE__), 'config/settings')
+    @@config = YAML.load_file(config_file)
+    @@config
+  end
+end
+
 post '/send' do
+  halt(401, "Texto no válido") unless captcha_pass?
   p = Prowler.new(config['username'], config['password'])
   p.send_notification(params['from'] || 'unknown',params['subject'] || 'no subject', params['text'] || 'no text')
   haml :sent
 end
 
 get '/' do
+  @apptitle = ProwlSMS::config['apptitle'] || 'ProwlSMS by Sergio Rubio"
   haml :index
 end
